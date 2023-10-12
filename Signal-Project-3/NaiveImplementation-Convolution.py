996bd1732389d804b180850ac179658a708e068c
Hanin Ainussyamsi Prabowo <haninsyamsi15@gmail.com>
2023-10-12 21:56:57 +0700

    Signal Project 3

--- Signal-Project-3/NaiveImplementation-Convolution.py
+++ Signal-Project-3/NaiveImplementation-Convolution.py
@@ -1,6 +1,3 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
 def convolve(Signal_1, Signal_2):
     # Hitung panjang sinyal yang dihasilkan
     length = len(Signal_1) + len(Signal_2) - 1
@@ -13,18 +10,13 @@ def convolve(Signal_1, Signal_2):
             if i - j >= 0 and i - j < len(Signal_2):
                 result[i] += Signal_1[j] * Signal_2[i - j]
     return result
+
+print ("Naive Impelementation of Convolution")
+print ("Hanin Ainussyamsi Prabowo")
+print ("5009211036")
+
 # Uji fungsi di atas dengan menggunakan dua sinyal
-Signal_1 = [1, 3, 5]
-Signal_2 = [2, 4, 6]
+Signal_1 = [1, 3, 5, 7]
+Signal_2 = [2, 4, 6, 8]
 result = convolve(Signal_1, Signal_2)
 print(result)
-# Validasi hasilnya menggunakan NumPy Convolve
-np_result = np.convolve(Signal_1, Signal_2)
-print(np_result)
-# Plot hasil dari fungsi konvolusi
-plt.plot(Signal_1, label='Signal 1')
-plt.plot(Signal_2, label='Signal 2')
-plt.plot(result, label='Convolution Result')
-plt.plot(np_result, label='NumPy Convolution Result')
-plt.legend()
-plt.show()
